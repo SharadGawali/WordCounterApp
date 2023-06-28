2b0fc917fb6eeed65d8f8ad96b839433d29c332f
Sharad Gawali <sharadgawali56@gmail.com>
2023-06-28 13:58:59 +0530

    Updates

---
+++ precache-manifest.929e5779dea3ef9d6640e6fe140e029d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "671f95ff9a3b2cae8f98",
+    "url": "/WordCounterApp/static/css/main.efc17e7e.chunk.css"
+  },
+  {
+    "revision": "671f95ff9a3b2cae8f98",
+    "url": "/WordCounterApp/static/js/main.671f95ff.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/WordCounterApp/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "faf732f1f21984600605",
+    "url": "/WordCounterApp/static/js/2.faf732f1.chunk.js"
+  },
+  {
+    "revision": "2bf06c17d6e29109b643",
+    "url": "/WordCounterApp/static/js/runtime~main.2bf06c17.js"
+  },
+  {
+    "revision": "c4cab9c68461db8f6cc071aa1efd8527",
+    "url": "/WordCounterApp/index.html"
+  }
+];
